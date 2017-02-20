699e70722b06a4a7ee5d9ecb5e2c8699ca13b566
mabedo <mabedo@inf1-mabedo.fjeclot.net>
2017-02-20 20:41:45 +0100

    "Prova d'IC amb Jenkins"

--- src/jenkins2/Jenkins2.java
+++ src/jenkins2/Jenkins2.java
@@ -1,21 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package jenkins2;
-
+package jenkins;
 /**
  *
- * @author mabedo
+ * @author dc1415daw2
  */
-public class Jenkins2 {
-
+import java.io.*;
+public class Jenkins {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        File f = new File("index.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Treballant amb l'IC Jenkins");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    Projecte per provar l'IC Jenkins");bw.newLine();
+            bw.write("    Modificació del projecte jenkins");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
     }
-    
 }
